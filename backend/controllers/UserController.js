0eea6425ddf42eca7a3a6961ac6ecbf837a47956
Pablo Marinho <pablolucas-1@outlook.com>
2025-07-12 10:51:24 -0300

    feat: criada a funcionalidade para cadastrar e logar um usuário, com encriptação de senha e token de acesso

---
+++ backend/controllers/UserController.js
@@ -0,0 +1,110 @@
+const bcrypt = require("bcrypt");
+
+const User = require("../models/User");
+const createUserToken = require("../helpers/create-user-token");
+
+module.exports = class UserController {
+  static async register(req, res) {
+    const { name, email, password, confirmPassword, phone } = req.body;
+
+    // validations
+    if (!name) {
+      res.status(422).json({ message: "O nome é obrigatório!" });
+      return;
+    }
+
+    if (!email) {
+      res.status(422).json({ message: "O e-mail é obrigatório!" });
+      return;
+    }
+
+    if (!password) {
+      res.status(422).json({ message: "A senha é obrigatória!" });
+      return;
+    }
+
+    if (!confirmPassword) {
+      res
+        .status(422)
+        .json({ message: "A confirmação de senha é obrigatória!" });
+      return;
+    }
+
+    if (!phone) {
+      res.status(422).json({ message: "O telefone é obrigatório!" });
+      return;
+    }
+
+    if (password !== confirmPassword) {
+      res.status(422).json({
+        message: "A senha e a confirmação de senha precisam ser identicas!",
+      });
+      return;
+    }
+
+    // check if user exists
+    const userExists = await User.findOne({ email: email });
+
+    if (userExists) {
+      res.status(422).json({ message: "Por favor, utilize outro e-mail!" });
+      return;
+    }
+
+    // create a password
+    const salt = await bcrypt.genSalt(12);
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const user = new User({
+      name,
+      email,
+      phone,
+      password: passwordHash,
+    });
+
+    try {
+      const newUser = await user.save();
+
+      await createUserToken(newUser, req, res);
+    } catch (error) {
+      res.status(500).json({ message: error });
+    }
+  }
+
+  static async login(req, res) {
+    const { email, password } = req.body;
+
+    if (!email) {
+      res.status(422).json({ message: "O e-mail é obrigatório!" });
+      return;
+    }
+
+    if (!password) {
+      res.status(422).json({ message: "O e-mail é obrigatório!" });
+      return;
+    }
+
+    // check if user exists
+    const user = await User.findOne({ email: email });
+
+    if (!user) {
+      res
+        .status(422)
+        .json({ message: "Não há usuário cadastrado com este e-mail!" });
+      return;
+    }
+
+    // check if password match with db password
+    const checkPassword = await bcrypt.compare(password, user.password);
+
+    if (!checkPassword) {
+      res.status(422).json({ message: "Senha inválida!" });
+      return;
+    }
+
+    try {
+      await createUserToken(user, req, res);
+    } catch (error) {
+      res.status(500).json({ message: error });
+    }
+  }
+};
---
+++ backend/helpers/create-user-token.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+const createUserToken = async (user, req, res) => {
+  // create a token
+  const token = jwt.sign(
+    {
+      name: user.name,
+      id: user._id,
+    },
+    "nossosecret"
+  );
+
+  // return token
+  res.status(200).json({
+    message: "Você está autenticado",
+    token: token,
+    userId: user._id,
+  });
+};
+
+module.exports = createUserToken;
